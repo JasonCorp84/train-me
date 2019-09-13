a4e7bcd2abc36cc1ea52c50aad887869c69e9c52
JasonCorp84 <csababalogh84@gmail.com>
2019-09-12 20:04:59 -0400

    font-end initiated

---
+++ my-trainer/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import 'typeface-roboto';
+import Navigation from './components/navigation';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
